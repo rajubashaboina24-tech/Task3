f1e7c2f7a93c2ef09bc07c39cd027a529451e4e1
Raju Bashaboina <rajubashaboina24@gmail.com>
2026-09-13 12:38:49 +0530

    Refactor task3
    Removed unnecessary comments and cleaned up code structure.

--- task3-cleaning.py
+++ task3-cleaning.py
@@ -1,22 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
-
-
-# In[17]:
-
 
 df = pd.read_csv("C:\\Users\\Welcome\\Downloads\\10,000 records healthcare data.csv")
 df.head()
-
-
-# In[19]:
-
-
 missing = pd.DataFrame({
     "Column": df.columns,
     "Missing Values": df.isnull().sum()
@@ -24,26 +9,15 @@ missing = pd.DataFrame({
 
 display(missing)
 
-
-# In[21]:
-
-
 if missing["Missing Values"].sum() == 0:
     print("Completeness: PASS")
 else:
     print("Completeness: FAIL")
 
 
-# In[39]:
-
-
 duplicate_names = df[df["Name"].duplicated()]
 
 display(duplicate_names)
-
-
-# In[35]:
-
 
 duplicate_names = df[""].duplicated().sum()
 
